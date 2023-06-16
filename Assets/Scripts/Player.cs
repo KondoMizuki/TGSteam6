96b4ff6754576527bee8d500a39fe3c43934164b
KondoMizuki <yukkuriomuretu3000@gmail.com>
2023-06-16 14:17:25 +0900

    基礎戦闘
    基本戦闘のプログラムを作りました。

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class Player : MonoBehaviour
 {
+    public int PlayerLife = 10;
+
+    public int PlayerAttack = 5;
+
+    public int DFE = 5;
+
+    public int Spd = 5;
+
+    public Text NameText;
+
+    public Text PlayerLifeText;
+
+  
+
+    public string playername;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        NameText.text = "プレイヤー";
+        playername = "プレイヤー";
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlayerLifeText.text = "体力 " + PlayerLife;
         
     }
 }
