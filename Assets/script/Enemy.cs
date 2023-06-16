5840df5ff17fe074a36b3d0439a9ca5d8848c101
KondoMizuki <yukkuriomuretu3000@gmail.com>
2023-06-16 14:29:36 +0900

    基本戦闘
    基本の戦闘システムを作りました

---
+++ Assets/script/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Enemy : MonoBehaviour
+{
+    public int EnemyLife = 5;
+
+    public int Attack = 7;
+
+    public int DFE = 3;
+
+    public int SPD = 1;
+
+   
+
+    //public Text EnemyLifeText;
+
+    public string Enemyname;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemyname = "ƒSƒuƒŠƒ“";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
