30ac6f0242a2c1b11b148e27dcf97b1d75f38ee5
KondoMizuki <yukkuriomuretu3000@gmail.com>
2023-06-20 13:31:11 +0900

    フロア機能を実装しようとしている

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -20,12 +20,14 @@ public class GameManager : MonoBehaviour
 
     public float _timeElaped;
 
+    public int floor = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         _repeatSpan = 4;//行動が行われるまでの時間
         _timeElaped = 2;//初期の行動までのやつ
-        
+
     }
 
     // Update is called once per frame
@@ -55,50 +57,63 @@ public class GameManager : MonoBehaviour
 
         }
 
-        
-       
+        if(playerManager.PlayerLife > 0 &&enemyManeger.EnemyLife == 0)
+        {
+            floor = floor + 1;
+        }
+
     }
 
-    public void rad()
-    {
-        int rad = Random.Range(0, 2);
-    }
+   
 
     public void battle()
     {
-        if (playerManager.SPD > enemyManeger.SPD)
+
+        if (playerManager.SPD > enemyManeger.SPD)//早いほうから行動
         {
             player();
             Invoke(nameof(Enemy), 2.0f);
         }
         else
-            if(enemyManeger.SPD > playerManager.SPD)
         {
             Enemy();
             Invoke(nameof(player), 2.0f);
         }
-       
+
 
 
     }
 
     void player()
     {
-        PlayerDamage = playerManager.PlayerAttack - enemyManeger.DFE;
-        if(playerManager.PlayerLife > 0 && enemyManeger.EnemyLife > 0)
+        int rad = Random.Range(0, 101);
+        if (rad >= 95)
+        {
+            PlayerDamage = playerManager.PlayerAttack * 2;
+        }
+        else
+        {
+            PlayerDamage = playerManager.PlayerAttack - enemyManeger.DFE;
+        }
+        if (playerManager.PlayerLife > 0 && enemyManeger.EnemyLife > 0)
         {
             enemyManeger.EnemyLife = enemyManeger.EnemyLife - PlayerDamage;
             logManeger.Damage1();
         }
+        
     }
     void Enemy()
     {
         EnemyDamage = enemyManeger.Attack - playerManager.DFE;
-        if(enemyManeger.EnemyLife > 0 && playerManager.PlayerLife > 0)
+        if (enemyManeger.EnemyLife > 0 && playerManager.PlayerLife > 0)
         {
             playerManager.PlayerLife = playerManager.PlayerLife - EnemyDamage;
             logManeger.Damage2();
         }
     }
+
+   
     
 }
+    
+
