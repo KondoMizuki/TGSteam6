f6b3bb5e56fc213e2f1352be00e45f76e2938f8a
kawakamitakuma0502 <129479277+kawakamitakuma0502@users.noreply.github.com>
2023-07-14 10:36:37 +0900

    Revert "ターンを増やす"
    This reverts commit 5af495613c84055a0fde5d32f20bc5287f5bcce3, reversing
    changes made to 6e5768db64a2d77d039d9854f1a88407997f6ceb.

--- Assets/script/EnemyGenerator.cs
+++ Assets/script/EnemyGenerator.cs
@@ -11,6 +11,11 @@ public class EnemyGenerator : MonoBehaviour
     public Enemys enemy3;
     public Enemys enemy4;
     public Enemys enemy5;
+    public Enemys enemy6;
+    public Enemys enemy7;
+    public Enemys enemy8;
+    public Enemys enemy9;
+    public Enemys enemy10;
 
     public bool isFirst = true;
 
@@ -25,26 +30,52 @@ public class EnemyGenerator : MonoBehaviour
     {
         if(gamemanager.stage == 1 && isFirst == true)
         {
-            Enemys enemycopy1 = Instantiate<Enemys>(enemy1);
+            Enemys enemycopy = Instantiate<Enemys>(enemy1);
             isFirst = false;
-        }else if(gamemanager.stage == 2 && isFirst == true)
+        }
+        else if(gamemanager.stage == 2 && isFirst == true)
         {
-            Enemys enemycopy2 = Instantiate<Enemys>(enemy2);
+            Enemys enemycopy = Instantiate<Enemys>(enemy2);
             isFirst = false;
         }
         else if (gamemanager.stage == 3 && isFirst == true)
         {
-            Enemys enemycopy3 = Instantiate<Enemys>(enemy3);
+            Enemys enemycopy = Instantiate<Enemys>(enemy3);
             isFirst = false;
         }
         else if (gamemanager.stage == 4 && isFirst == true)
         {
-            Enemys enemycopy4 = Instantiate<Enemys>(enemy4);
+            Enemys enemycopy = Instantiate<Enemys>(enemy4);
             isFirst = false;
         }
         else if (gamemanager.stage == 5 && isFirst == true)
         {
-            Enemys enemycopy5 = Instantiate<Enemys>(enemy5);
+            Enemys enemycopy = Instantiate<Enemys>(enemy5);
+            isFirst = false;
+        }
+        else if (gamemanager.stage == 6 && isFirst == true)
+        {
+            Enemys enemycopy = Instantiate<Enemys>(enemy6);
+            isFirst = false;
+        }
+        else if (gamemanager.stage == 7 && isFirst == true)
+        {
+            Enemys enemycopy = Instantiate<Enemys>(enemy7);
+            isFirst = false;
+        }
+        else if (gamemanager.stage == 8 && isFirst == true)
+        {
+            Enemys enemycopy = Instantiate<Enemys>(enemy8);
+            isFirst = false;
+        }
+        else if (gamemanager.stage == 9 && isFirst == true)
+        {
+            Enemys enemycopy = Instantiate<Enemys>(enemy9);
+            isFirst = false;
+        }
+        else if (gamemanager.stage == 10 && isFirst == true)
+        {
+            Enemys enemycopy = Instantiate<Enemys>(enemy10);
             isFirst = false;
         }
     }
