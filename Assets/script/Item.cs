ad7e6b09fb0720212dcd21abdb3f5cda4f912423
KondoMizuki <yukkuriomuretu3000@gmail.com>
2023-07-14 10:56:56 +0900

    Revert "Revert "Revert "Revert "Revert "Revert "ターンを増やす""""""
    This reverts commit a543b0151f086ca173371d5203d8c449a044a198.

--- Assets/script/Item.cs
+++ Assets/script/Item.cs
@@ -11,22 +11,12 @@ public class Item : MonoBehaviour
     public ItemData I_Data3;
     public ItemData I_Data4;
     public ItemData I_Data5;
-    public ItemData I_Data6;
-    public ItemData I_Data7;
-    public ItemData I_Data8;
-    public ItemData I_Data9;
-    public ItemData I_Data10;
 
     public int SellingPrice1;
     public int SellingPrice2;
     public int SellingPrice3;
     public int SellingPrice4;
     public int SellingPrice5;
-    public int SellingPrice6;
-    public int SellingPrice7;
-    public int SellingPrice8;
-    public int SellingPrice9;
-    public int SellingPrice10;
 
     public string id;          //“o˜^ID
 
@@ -68,36 +58,6 @@ public class Item : MonoBehaviour
                 Itemname = I_Data5.Itemname;
                 Dropprobability = I_Data5.Dropprobability;
                 break;
-
-            case 6:
-                id = I_Data6.id;
-                Itemname = I_Data6.Itemname;
-                Dropprobability = I_Data6.Dropprobability;
-                break;
-
-            case 7:
-                id = I_Data7.id;
-                Itemname = I_Data7.Itemname;
-                Dropprobability = I_Data7.Dropprobability;
-                break;
-
-            case 8:
-                id = I_Data8.id;
-                Itemname = I_Data8.Itemname;
-                Dropprobability = I_Data8.Dropprobability;
-                break;
-
-            case 9:
-                id = I_Data9.id;
-                Itemname = I_Data9.Itemname;
-                Dropprobability = I_Data9.Dropprobability;
-                break;
-
-            case 10:
-                id = I_Data10.id;
-                Itemname = I_Data10.Itemname;
-                Dropprobability = I_Data10.Dropprobability;
-                break;
         }
 
         SellingPrice1 = I_Data.SellingPrice;
@@ -105,11 +65,6 @@ public class Item : MonoBehaviour
         SellingPrice3 = I_Data3.SellingPrice;
         SellingPrice4 = I_Data4.SellingPrice;
         SellingPrice5 = I_Data5.SellingPrice;
-        SellingPrice6 = I_Data.SellingPrice;
-        SellingPrice7 = I_Data2.SellingPrice;
-        SellingPrice8 = I_Data3.SellingPrice;
-        SellingPrice9 = I_Data4.SellingPrice;
-        SellingPrice10 = I_Data5.SellingPrice;
     }
 
     // Update is called once per frame
