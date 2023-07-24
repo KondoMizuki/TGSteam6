bb28ee6130a0750578e1fed8758dafa87dd330e9
KondoMizuki <yukkuriomuretu3000@gmail.com>
2023-07-24 11:41:35 +0900

    いどうできるよー

--- Assets/script/Shop.cs
+++ Assets/script/Shop.cs
@@ -9,11 +9,13 @@ public class Shop : MonoBehaviour
     public Equipment equipment;
     public  Player player;
     public int count;
-    public int upcount;
+    public int upcount = 1;
     public Text SwordText;
     public Text ShildText;
     public Text ShoesText;
     public Text heartText;
+    public Text backText;
+    public Text sellText;
    
 
     // Start is called before the first frame update
@@ -25,13 +27,13 @@ public class Shop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (count > 1)//カーソル移動
+        if (count > 2)//カーソル移動
         {
-            count = count = 1;
+            count = count = 2;
         }
-        if (count < 0)
+        if (count < -2)
         {
-            count = count = 0;
+            count = count = -2;
         }
 
         if(upcount > 0)
@@ -43,57 +45,85 @@ public class Shop : MonoBehaviour
             upcount = -1;
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             count = count + 1;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             count = count - 1;
         }
-        if(Input.GetKey(KeyCode.UpArrow))
+        if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             upcount = upcount + 1; 
         }
-        if(Input.GetKey(KeyCode.DownArrow))
+        if(Input.GetKeyDown(KeyCode.DownArrow))
         {
             upcount = upcount - 1;
         }
 
 
-        if (count == 0 && upcount == 0)
+        if (upcount == 0 && count == 0 )
         {
             SwordText.fontSize = 30;
             ShildText.fontSize = 20;
             ShoesText.fontSize = 20;
             heartText.fontSize = 20;
+            sellText.fontSize = 20;
+            backText.fontSize = 20;
         }
         else
-            if (count == 1 && upcount == 0)
+        if (count == 1 )
         {
             SwordText.fontSize = 20;
             ShildText.fontSize = 30;
             ShoesText.fontSize = 20;
             heartText.fontSize = 20;
+            sellText.fontSize = 20;
+            backText.fontSize = 20;
         }
         else
-          if (count == 0 && upcount == -1)
+        if (count == -1)
         {
             SwordText.fontSize = 20;
             ShildText.fontSize = 20;
             ShoesText.fontSize = 30;
             heartText.fontSize = 20;
+            sellText.fontSize = 20;
+            backText.fontSize = 20;
         }
         else
-          if (count == 1 && upcount == -1)
+        if (upcount == -1 && count == 0)
         {
             SwordText.fontSize = 20;
             ShildText.fontSize = 20;
             ShoesText.fontSize = 20;
             heartText.fontSize = 30;
+            sellText.fontSize = 20;
+            backText.fontSize = 20;
+        }
+        else
+        if (count == 2)
+        {
+            SwordText.fontSize = 20;
+            ShildText.fontSize = 20;
+            ShoesText.fontSize = 20;
+            heartText.fontSize = 20;
+            sellText.fontSize = 30;
+            backText.fontSize = 20;
+        }
+        if(count == -2)
+        {
+            SwordText.fontSize = 20;
+            ShildText.fontSize = 20;
+            ShoesText.fontSize = 20;
+            heartText.fontSize = 20;
+            sellText.fontSize = 20;
+            backText.fontSize = 30;
         }
 
-        if (count == 0 && upcount == 0 && Input.GetKeyDown(KeyCode.Space))
+
+        if (count == 0  && Input.GetKeyDown(KeyCode.Space))
         {
             player.MaxLife = player.MaxLife + equipment.MaxLife;
             player.PlayerAttack = player.PlayerAttack + equipment.Attack;
@@ -102,7 +132,7 @@ public class Shop : MonoBehaviour
 
         }
         else
-            if (count == 1 && upcount == 0 && Input.GetKeyDown(KeyCode.Space))
+            if (count == 1 && Input.GetKeyDown(KeyCode.Space))
         {
             player.MaxLife = player.MaxLife + equipment.MaxLife;
             player.PlayerAttack = player.PlayerAttack + equipment.Attack;
@@ -111,7 +141,7 @@ public class Shop : MonoBehaviour
 
         }
         else
-            if (count == 0 && upcount == -1 && Input.GetKeyDown(KeyCode.Space))
+            if (upcount == 1 && Input.GetKeyDown(KeyCode.Space))
         {
             player.MaxLife = player.MaxLife + equipment.MaxLife;
             player.PlayerAttack = player.PlayerAttack + equipment.Attack;
@@ -120,7 +150,7 @@ public class Shop : MonoBehaviour
 
         }
         else
-            if (count == 1 && upcount == -1 && Input.GetKeyDown(KeyCode.Space))
+            if (upcount == -1 && Input.GetKeyDown(KeyCode.Space))
         {
             player.MaxLife = player.MaxLife + equipment.MaxLife;
             player.PlayerAttack = player.PlayerAttack + equipment.Attack;
