54a657fc6fef02cce2fcf252b7d85e7c7ffbeb0f
kawakamitakuma0502 <129479277+kawakamitakuma0502@users.noreply.github.com>
2023-08-03 10:58:39 +0900

    共産主義者打倒

--- Assets/script/InventoryManaget.cs
+++ Assets/script/InventoryManaget.cs
@@ -18,7 +18,7 @@ public class InventoryManaget : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PossessionItemText.text = "Š•i:" + "\n";
+        PossessionItemText.text = "Š•i\n";
 
         InventoryPanel.SetActive(false);
 
