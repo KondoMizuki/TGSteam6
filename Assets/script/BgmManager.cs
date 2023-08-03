34ed718b6bd604b695bd0b4f079144cf57805de5
kawakamitakuma0502 <129479277+kawakamitakuma0502@users.noreply.github.com>
2023-08-03 13:26:04 +0900

    オプション追加

--- Assets/script/BgmManager.cs
+++ Assets/script/BgmManager.cs
@@ -6,11 +6,32 @@ using UnityEngine.UI;
 public class BgmManager : MonoBehaviour
 {
     public Slider slider;
+    public GameObject MuteBt;
+    public GameObject VoluemBt;
     AudioSource audioSource;
+
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         slider.onValueChanged.AddListener(value => this.audioSource.volume = value);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void VoluemButton()
+    {
+        VoluemBt.SetActive(false);
+        MuteBt.SetActive(true);
+        AudioListener.volume = 0f;
+    }
+    public void MuteButton()
+    {
+        MuteBt.SetActive(false);
+        VoluemBt.SetActive(true);
+        AudioListener.volume = 1f;
     }
 }
